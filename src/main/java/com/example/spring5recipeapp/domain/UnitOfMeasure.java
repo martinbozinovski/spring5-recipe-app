2002d0814637c9e33725a8ad71e971b48b576479
martinbozinovski <bozinovskimartin@gmail.com>
2019-12-14 12:08:15 +0100

    successful run on IT test

--- src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
@@ -1,10 +1,13 @@
 package com.example.spring5recipeapp.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class UnitOfMeasure {
 //unidirectional relationship from Ingredient to UnitOfMeasure
@@ -13,20 +16,4 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
